ed034423891409c2afe565f677ddc459b63a86a4
Ryan Ingram <ry_ingram@hotmail.com>
2016-05-24 19:57:23 -0400

    Initial Commit of Bootstrapper Sample Project

---
+++ BootstraperSample/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using BootstraperSample.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace BootstraperSample.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
